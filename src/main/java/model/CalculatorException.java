5031c2572e5b7bf45a0de718a8ef9224bcc3d4e4
Alexandr <gp73leopard@gmail.com>
2021-12-15 02:14:02 +0400

    twentieth commit

--- src/main/java/model/CalculatorException.java
+++ src/main/java/model/CalculatorException.java
@@ -1,7 +1,5 @@
 package model;
 
-import sun.misc.FloatingDecimal;
-
 public class CalculatorException extends RuntimeException{
 
     public CalculatorException(String s) throws NumberFormatException {
