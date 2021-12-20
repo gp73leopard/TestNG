fd3242c53884cf23238f2c462a144a6a023d963e
Alexandr <gp73leopard@gmail.com>
2021-12-20 15:25:46 +0400

    twenty sixth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,33 @@
 import model.Calculator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        mainTest();
+        String[] str = mainTest();
+
+        System.out.println(Calculator.execute(str));
     }
 
-    public static void mainTest(){
+    public static String[] mainTest(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите знак: ");
+        String a = sc.nextLine();
+        System.out.print("Введите первое число: ");
+        String b = sc.nextLine();
+        System.out.print("Введите второе число: ");
+        String c = sc.nextLine();
+
         String[] str = {
-                "/",
-                "25",
-                "5",
-                "5"
+                a,
+                b,
+                c
         };
-        System.out.println(Calculator.execute(str));
+
+        return str;
+
+
     }
 }
--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -4,22 +4,26 @@ public class Calculator {
     public static String execute(String[] params) {
 
         // Проверка на null
-        if(params[1]==null || params[2]==null)
-        { throw new CalculatorException("Среди чисел есть пустое поле null");}
+        if (params[1] == null || params[2] == null) {
+            throw new CalculatorException("Среди чисел есть пустое поле null");
+        }
 
         // Проверка на то может ли входной параметр быть преобразован в double
-        try{Double.parseDouble(params[1]);}
-        catch (NumberFormatException ex){
+        try {
+            Double.parseDouble(params[1]);
+        } catch (NumberFormatException ex) {
             throw new CalculatorException("Первое значение не является числом");
         }
         // Аналогичная проверка для второго
-        try{Double.parseDouble(params[2]);}
-        catch (NumberFormatException ex){
+        try {
+            Double.parseDouble(params[2]);
+        } catch (NumberFormatException ex) {
             throw new CalculatorException("Второе значение не является числом");
         }
 
         double a = Double.parseDouble(params[1]);
         double b = Double.parseDouble(params[2]);
+        char d = params[0].charAt(0);
 
         // Общая проверка на слишком большое и слишком малое число, деление на 0 и проверка арифметического знака
         try {
@@ -27,20 +31,21 @@ public class Calculator {
             double c1 = a / b;
             double c2 = a - b;
             double c3 = a * b;
-            if (params[0] == "+" && b != 0 && c >= -2147483648 && c <= 2147483647 ) {
+            if (d == '+' && b != 0 && c >= -2147483648 && c <= 2147483647) {
                 return Double.toString(c);
-            } else if (params[0] == "/" && b != 0 && c <= 2147483647 && c >= -2147483648) {
+            } else if (d == '/' && b != 0 && c <= 2147483647 && c >= -2147483648) {
                 return Double.toString(c1);
-            } else if (params[0] == "-" && b != 0 && c >= -2147483648 && c <= 2147483647) {
+            } else if (d == '-' && b != 0 && c >= -2147483648 && c <= 2147483647) {
                 return Double.toString(c2);
-            } else if (params[0] == "*" && b != 0 && c >= -2147483648 && c <= 2147483647) {
+            } else if (d == '*' && b != 0 && c >= -2147483648 && c <= 2147483647) {
                 return Double.toString(c3);
             } else {
                 throw new CalculatorException("Проверьте правильность параметров");
             }
+        } catch (CalculatorException ex) {
+            throw new CalculatorException(ex.getMessage());
         }
-            catch (CalculatorException ex){
-                throw new CalculatorException(ex.getMessage());
-            }
+
     }
 }
+
